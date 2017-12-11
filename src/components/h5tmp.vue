4d951dffc8489079a5b24aef7e4771c5dd34441f
luckyzd <zhangdi5@xiaomi.com>
2017-12-11 17:47:59 +0800

    debug

--- src/components/h5tmp.vue
+++ src/components/h5tmp.vue
@@ -155,6 +155,7 @@
         totalScore: 0,
         progressValue: 0,
         btnS: 1,
+        wxData_share: null,
       };
     },
     // 在实例初始化之后，数据观测 (data observer) 和 event/watcher 事件配置之前被调用。
@@ -252,16 +253,16 @@
     },
     methods: {
       initWxShare() {
-
+        const self = this;
         $.get("/wx/get_wxData_share", {}, function (data, status, xhr) {
           console.log(data);
-
+          self.wxData_share = data.data;
           wx.config({
-            debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: 'wx43d17d7d013dc48f', // 必填，公众号的唯一标识
-            timestamp: data.timestamp, // 必填，生成签名的时间戳
-            nonceStr: data.nonceStr, // 必填，生成签名的随机串
-            signature: data.signature,// 必填，签名，见附录1
+            timestamp: self.wxData_share.timestamp, // 必填，生成签名的时间戳
+            nonceStr: self.wxData_share.nonceStr, // 必填，生成签名的随机串
+            signature: self.wxData_share.signature,// 必填，签名，见附录1
             jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareQZone', 'onMenuShareWeibo'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
           });
 
