c78b832269f112224df0f2448e7fef10ca83f52f
luckyzd <zhangdi5@xiaomi.com>
2017-12-11 23:04:25 +0800

    JS判断是否在微信浏览器打开 todo

--- src/components/h5tmp.vue
+++ src/components/h5tmp.vue
@@ -180,10 +180,8 @@
       //   // Code that will run only after the
       //   // entire view has been rendered
       // });
-
-
-
       const self = this;
+      self.initWxShare();
       // img preload
       function imgPreload(imgList, cbk) {
         let count = 0;
@@ -248,8 +246,57 @@
 
       // obj.p && (self.curPage = obj.p);
 
+      // JS判断是否在微信浏览器打开
+      try {
+        if (browser.versions.mobile) {//判断是否是移动设备打开。browser代码在下面
+          var ua = navigator.userAgent.toLowerCase();//获取判断用的对象
+          if (ua.match(/MicroMessenger/i) == "micromessenger") {
+            //在微信中打开
+            console.log('在微信中打开');
+          }
+          if (ua.match(/WeiBo/i) == "weibo") {
+            //在新浪微博客户端打开
+            console.log('在新浪微博客户端打开');
+          }
+          if (ua.match(/QQ/i) == "qq") {
+            //在QQ空间打开
+            console.log('在QQ空间打开');
+          }
+          if (browser.versions.ios) {
+            //是否在IOS浏览器打开
+            console.log('是否在IOS浏览器打开');
+          }
+          if (browser.versions.android) {
+            //是否在安卓浏览器打开
+            console.log('是否在安卓浏览器打开');
+          }
+        } else {
+          //否则就是PC浏览器打开
+          console.log('否则就是PC浏览器打开');
+        }
 
-      self.initWxShare();
+        var browser = {
+          versions: function () {
+            var u = navigator.userAgent, app = navigator.appVersion;
+            return {         //移动终端浏览器版本信息
+              trident: u.indexOf('Trident') > -1, //IE内核
+              presto: u.indexOf('Presto') > -1, //opera内核
+              webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+              gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+              mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+              ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+              android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或uc浏览器
+              iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
+              iPad: u.indexOf('iPad') > -1, //是否iPad
+              webApp: u.indexOf('Safari') == -1 //是否web应该程序，没有头部与底部
+            };
+          }(),
+          language: (navigator.browserLanguage || navigator.language).toLowerCase()
+        }
+
+      } catch (error) {
+        console.log(error)
+      }
     },
     methods: {
       initWxShare() {
