c3d18d4e99b3e3df536a86a2dd53b027dc173b8b
luckyzd <zhangdi5@xiaomi.com>
2018-03-07 14:36:26 +0800

    houlaide

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,34 @@
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
     <!-- <HelloWorld/> -->
-    <h5tmp/>
+    <!-- <h5tmp/> -->
+    <cors/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-import h5tmp from './components/h5tmp'
+  import HelloWorld from './components/HelloWorld'
+  import h5tmp from './components/h5tmp'
+  import cors from './components/CORS'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld,
-    h5tmp
+  export default {
+    name: 'app',
+    components: {
+      HelloWorld,
+      h5tmp,
+      cors
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  /* margin-top: 60px; */
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    /* margin-top: 60px; */
+  }
+
 </style>
